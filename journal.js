c4951e9c8bb0d7192fed22e09451396030f10d84
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-06-12 08:51:49 +0300

    I created three objects of entries

--- journal.js
+++ journal.js
@@ -1,1 +1,21 @@
 console.log('it works');
+
+// Create objects of entries
+const firstEntry = {
+  title: "Revenue journal",
+  content: "When goods or services are sold on credit, debit accounts receivable and credit sales. If a sale is for cash, then the debit is to the cash account instead of the accounts receivable account.",
+};
+
+const secondEntry = {
+  title: "Lorem journal",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, quam illum odio consectetur quisquam, ipsum atque quidem excepturi tempora vero sint obcaecati. Eius, modi architecto quos blanditiis at illo quisquam?"
+};
+
+const lastEntry = {
+  title: "Last journal",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, quam illum odio consectetur quisquam, ipsum atque quidem excepturi tempora vero sint obcaecati. Eius, modi architecto quos blanditiis at illo quisquam?"
+};
+
+let yourOption = prompt(`Choose (1) for listing the all entries,
+Choose (2) to add a new entry,
+Choose (3) to quit`);
